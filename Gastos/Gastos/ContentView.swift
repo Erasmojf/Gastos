241e38e0de0a1739142bcaa8d24205365c132ee2
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2022-11-15 17:19:42 -0300

    Finalizando o App Gastos

--- Gastos/Gastos/ContentView.swift
+++ Gastos/Gastos/ContentView.swift
@@ -8,8 +8,13 @@
 //  Fidju de Bideira de Feira de Caracol
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
+    //var demoData: [Double] = [8,2,4,6,12,9,2]
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -19,6 +24,21 @@ struct ContentView: View {
                         .font(.title2)
                         .bold()
                     
+                    //MARK: Chart
+                    let data = transactionListVM.accumulateTransactions()
+                    let totalExpenses = data.last?.1 ?? 0
+                    
+                    CardView {
+                        VStack(alignment: .leading) {
+                            ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title)
+                            LineChart()
+                        }
+                        .background(Color.systemBackground)
+                    }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                            .frame(height: 300)
+   
                     //MARK: Transaction List
                     RecentTransactionList()
                 }
